f24ae4834dd2fb0ba9227a80a3445bbb82191d43
Daniil <danilapavlovsiy056@gmail.com>
2025-07-02 11:11:36 +0300

    user profile

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -4,8 +4,6 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
-
-
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
